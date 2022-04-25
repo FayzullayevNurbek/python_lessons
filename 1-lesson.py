c71881ca3d58d4b58c332f46adfb30ca5a6e21b3
Nurbek <104354561+FayzullayevNurbek@users.noreply.github.com>
2022-04-25 15:15:35 +0500

    added new file in Gidhub
    I loaded my first files in Github account

---
+++ 1-lesson.py
@@ -0,0 +1,12 @@
+
+import math
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+if (a+b)>c and (a+c)>b and (c+b)>a:
+   if a==b and b==c:
+       print(3)
+   elif (a==b and a!=c) or (a==c and b!=a):
+        print(2)   
+   else:print(1)
+else: print(0)    
